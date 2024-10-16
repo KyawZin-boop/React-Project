9ff6935b0a7c2fe01d186f9b3cb9de74d85ff8ef
Kyaw Zin Thant <kyawzinthant57@gmail.com>
2024-10-16 15:15:27 +0630

    speedInsights fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import SpeedInsights from '@vercel/insights';
 import App from './App.jsx'
 import './weatherStyle.css'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
